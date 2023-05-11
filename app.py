3c5a90383c05702096577241b956f91502708471
Ubuntu <ubuntu@ip-172-31-21-215.ec2.internal>
2023-05-11 21:29:17 +0000

    add

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def start_cpu_stress():
+    # Start a separate process for running stress_cpu.py
+    subprocess.Popen(['python', 'stress_cpu.py'])
+    return 'CPU stress started'
+
+@app.route('/', methods=['GET'])
+def get_private_ip():
+    # Get the private IP address of the EC2 instance
+    private_ip = socket.gethostbyname(socket.gethostname())
+    return f'Private IP: {private_ip}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
